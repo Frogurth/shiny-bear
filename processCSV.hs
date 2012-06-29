4754957479cc0caf7033783e01f80a11963b6332
Frogurth <artes.waldi@googlemail.com>
2012-06-29 18:09:14 +0200

    Implement error reporting of line with to few or to many values

--- processCSV.hs
+++ processCSV.hs
@@ -1,16 +1,31 @@
-module Frogurth.CSV(processCSV) where
+module Frogurth.CSV( processCSV ) where
 
-import Data.Map(Map, fromList)
+import Data.Map( Map, fromList )
+import Data.Maybe( fromJust )
+import Data.List( elemIndex )
+import Data.Either( Either(..), lefts, rights )
 
-splitOn :: Char -> String -> [String]
-splitOn _ "" = []
-splitOn d s = let str = takeWhile (/= d) s
-                  len = (length str) + 1
-                  rest = drop len s
-              in str : (splitOn d rest)
+splitKomma :: String -> [String]
+splitKomma "" = []
+splitKomma s = let str = takeWhile (/= ',') s
+                   len = (length str) + 1
+                   rest = drop len s
+               in str : (splitKomma rest)
 
-processCSV :: Char -> [String] -> [Map String String]
-processCSV d ls = let splitKomma = splitOn d
-                      h = splitKomma $ head ls
-                      mapping = fromList . zip h
-                  in map (mapping . splitKomma) (tail ls) 
+processCSV :: Char -> [String] -> Either [String] [Map String String]
+processCSV d ls = let h = splitKomma $ head ls
+                      results = map (parseLine h (tail ls)) (tail ls)
+                  in if lefts results == []
+                     then Right $ rights results
+                     else Left $ lefts results
+
+
+parseLine :: [String] -> [String] -> String -> Either String (Map String String)
+parseLine header ls line = let mapping = fromList . zip header
+                               splitLine = splitKomma line
+                               getIndex = show . (+2) $ fromJust $ elemIndex line ls
+                           in if length header > length splitLine
+                              then Left $ "to few values on line " ++ getIndex 
+                              else if length header < length splitLine
+                                   then Left $ "to many values on line " ++ getIndex  
+                                   else Right $ mapping splitLine
